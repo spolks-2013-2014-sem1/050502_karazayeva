e64ad261bb9ae4d5d4cffe13a9788f6d74a08025
DaryaAndreevna <karazayeva.darya@gmail.com>
2013-12-14 17:15:17 +0300

    TCP parallel transmission

--- lab_7/TCP/Server.rb
+++ lab_7/TCP/Server.rb
@@ -11,38 +11,32 @@ begin
   server = TCP.createServer(host, port)
   client  = TCP.createSocket
 
-    loop do 
-       timeout(20) do
-         client, = server.accept
-         puts client
-       end
+  loop do 
+    timeout(20) do
+       client, = server.accept
+       puts client
+    end
  
-       Thread.new do
-         clients[Thread.current] = client
-         receive_size = 0
-         begin
-          
-           file =  File.open("#{client}", 'w')
+     Thread.new do
+       clients[Thread.current] = client
+       receive_size = 0
 
-           while data = clients[Thread.current].recv(CONST::SIZE)
-             if data.size < CONST::SIZE
-               puts "#{clients[Thread.current]} done"
-               file.close
-               break
-             end
-             receive_size += data.size
-             puts " :#{Thread.current} #{receive_size}"
-             file.write(data)
+       file =  File.open("#{client}", 'w')
+       while data = clients[Thread.current].recv(CONST::SIZE)
+         if data.size < CONST::SIZE
+           puts "#{clients[Thread.current]} done"
+           file.close
+           clients.delete(Thread.current)
+           break
          end
-     ensure
-       mutex.synchronize do
-         threads.delete(Thread.current)
+         receive_size += data.size
+         puts " :#{Thread.current} #{receive_size}"
+         file.write(data)
        end
      end
    end
    puts "New client"
- end
- 
+
 rescue Timeout::Error
   puts "No one wants to connect:("
 end
